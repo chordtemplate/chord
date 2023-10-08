94bbd16ffda347122df7487596b4e9539e3eb1c9
Ángel González Vázquez <contact@angelnext.dev>
2023-10-08 22:29:16 +0200

    🎉 feat(listeners): Add type checks to imported listeners

--- src/utils/listeners.ts
+++ src/utils/listeners.ts
@@ -6,7 +6,7 @@ import {
 	SlashCommandBuilder,
 } from "discord.js";
 import fg from "fast-glob";
-import { log } from "./logger";
+import { log } from "utils/logger";
 
 export const commands = new Collection<
 	string,
@@ -19,19 +19,31 @@ export async function setListeners(client: Client) {
 	for (const file of files) {
 		const { on, action }: ClientEvent = await import(file);
 
+		const isSlashCommand = on instanceof SlashCommandBuilder;
+		const isContextCommand = on instanceof ContextMenuCommandBuilder;
+
+		if (typeof on !== "string" && !isSlashCommand && !isContextCommand) {
+			throw new TypeError(
+				`Command at "${file}" is missing or is not exporting the "on" listener data`,
+			);
+		}
+
+		if (typeof action !== "function") {
+			throw new TypeError(
+				`Command at "${file}" is missing or is not exporting the "action" function`,
+			);
+		}
+
 		log.info(`Loaded ${file}`);
 
-		if (
-			on instanceof SlashCommandBuilder ||
-			on instanceof ContextMenuCommandBuilder
-		) {
+		if (isSlashCommand || isContextCommand) {
 			commands.set(on.name, [on, action]);
 			continue;
 		}
 
 		client.on(on, (x) => {
 			action(x).catch((err) => {
-				log.error(`Uncaught error at event on file "${file.slice(0, -3)}.ts"`);
+				log.error(`Uncaught error at event on file "${file}"`);
 				console.error(err);
 			});
 		});
